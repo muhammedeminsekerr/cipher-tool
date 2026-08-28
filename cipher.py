146bb9ec1dedf86f4c981200f9481ceaea3c3449
Muhammed Emin Şeker <muhammedeminsekerr@gmail.com>
2026-08-28 21:49:34 +0300

    Add cipher.py skeleton with TODO stubs

---
+++ cipher.py
@@ -0,0 +1,16 @@
+# Cipher Tool
+# Caesar ve Vigenere sifreleme araci
+
+def caesar(text, shift):
+    # TODO: her karakteri dolas, harfse kaydir, degilse dokunma
+    pass
+
+
+def vigenere(text, key, decrypt=False):
+    # TODO: anahtar kelimeden turetilen degisken shift ile sifrele/coz
+    pass
+
+
+if __name__ == "__main__":
+    # TODO: test ciktilari
+    pass
