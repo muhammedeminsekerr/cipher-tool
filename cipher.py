58763ee124b868c9f49e5ea556415b9b140e714c
Muhammed Emin Şeker <muhammedeminsekerr@gmail.com>
2026-08-28 21:51:15 +0300

    Implement Caesar cipher encryption/decryption,

--- cipher.py
+++ cipher.py
@@ -14,3 +14,12 @@ def vigenere(text, key, decrypt=False):
 if __name__ == "__main__":
     # TODO: test ciktilari
     pass
+def caesar(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+    return result
